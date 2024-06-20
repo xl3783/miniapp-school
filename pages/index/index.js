b137606d3ed4836241c417109c727610c8d41aa2
xiaolei <xiaolei2023@icloud.com>
2024-06-20 21:18:47 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+    menuItems: [
+      {name: '学院简介', icon: '/images/intro.png'},
+      {name: '招生简章', icon: '/images/enrollment_guide.png'},
+      {name: '招生计划', icon: '/images/enrollment_plan.png'},
+      {name: '院系专业', icon: '/images/departments.png'},
+      {name: '多彩校园', icon: '/images/campus.png'},
+      {name: '问卷调查', icon: '/images/survey.png'},
+      {name: '单招成绩查询', icon: '/images/results.png'},
+      {name: '联系我们', icon: '/images/contact.png'},
+    ]
+  }
+})
